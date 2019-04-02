ddb4c8098aacfb5c192ec3687cb0ca2019a44d3a
Alex Brearley <alexlbrearley@gmail.com>
2019-04-03 09:41:35 +1100

    Setting up

--- main.c
+++ main.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
 
+int menu();
+void rotation();
+void substitution();
+
 int main()
 {
-    printf();
+    printf("Welcome to Cipher program\n\n");
+    printf("This program has the following options:\n\n a)Rotation Cypher\n b)Substitution Cypher\n c)Decryption\n\n Please choose one:\n");
+    scanf();
+    
 return 0;
+}
+
+void rotation()
+{
+    
+    
+}
+
+void substitution()
+{
+    
+    
 }
