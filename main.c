38c5b786a5d074ce85067aa487390d60aa93dfca
Alex Brearley <alexlbrearley@gmail.com>
2019-04-03 09:59:41 +1100

    setting up a menu

--- main.c
+++ main.c
@@ -6,9 +6,23 @@ void substitution();
 
 int main()
 {
+    char selection;
     printf("Welcome to Cipher program\n\n");
     printf("This program has the following options:\n\n a)Rotation Cypher\n b)Substitution Cypher\n c)Decryption\n\n Please choose one:\n");
-    scanf();
+    scanf("%c", &c);
+    while(c < 'a' || c > 'c')
+    {
+        switch(c)
+        {
+            case 'a':
+            case 'b':
+            case 'c':
+            case 'd':
+            default: printf("Invalid option, please enter a), b), c) or d) : \n", c);
+
+        }
+        
+    }
     
 return 0;
 }
