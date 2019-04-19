4518e895230fb32b74758e4979168c2df5b8a867
Alex Brearley <alexlbrearley@gmail.com>
2019-04-19 17:02:53 +1000

    file io working

--- fileIOTest.c
+++ fileIOTest.c
@@ -3,51 +3,49 @@
 /*---This function takes the input text and converts everything to capital---*/
 void textCaps(char *strng);
 
-/* ---This function takes a variable x to rotate the value for each character---*/
-void encryptRotation(int x, char *strng);
-
-/*---This function takes encrypted text and the known value it was rotated by to decode for original text---*/
-void decryptRotation(int x, char *strng);
 
 /*---Main---*/
 int main()
 {
-    int x, y;
+    int x, y, i;
     char strng[30];
+    char ch;
+    FILE *in, *out;
+    char inputName[30];
     
-    FILE *input;
-    input = fopen("input.txt", "r+");
-    if (input == NULL)
+    printf("Enter input file name: ");
+    scanf("%s", inputName);
+    
+    
+    in = fopen(inputName, "r+");
+    if (in == NULL)         //prints user friendly error message
     {
-        perror("fopen()");
+        perror("Error : fopen()");
         return 0;
     }
-    printf("File contians: %s\n", input);
+    
+    for (i=0; i<30; i++)
+    {
+        if ( !(feof(in)) )
+        {
+            fscanf(in, "%c", &ch);
+            strng[i] = ch;
+        }
+    }
+    printf("File contains: %s\n", strng); //original file contents
+    
+    textCaps(strng); //convert to all caps
+    fprintf(in, "%s", strng);
+    for (i=0; i<30; i++)
+    {
+        if ( !(feof(in)) )
+        {
+            fscanf(in, "%c", &ch);
+            strng[i] = ch;
+        }
+    }
+    printf("File contains: %s\n", strng);
 
-    //fscanf("%s", strng);
-    
-    //textCaps(input);
-    
-    
-    printf("Please choose an option:\n1: Encrypt\n2: Decrypt\n");
-    scanf("%d", &y);
-    
-    if (y==1)
-        {
-            printf("Enter a rotation value: ");
-            scanf("%d", &x);
-            encryptRotation(x, strng);
-            //fprintf(FILE *output,"%s", strng);
-        }
-     else if (y==2)
-        {
-            printf("Enter a rotation value: ");
-            scanf("%d", &x);
-            printf("Enter a rotation value: ");
-            scanf("%d", &x);
-            decryptRotation(x, strng);
-            //fprintf(FILE *output,"%s", strng);
-        }
 }
 
 /*---Functions---*/
@@ -55,37 +53,8 @@ void textCaps(char *strng)
 {
     for(int i=0; i<30; i++)
     {
-        if (strng[i] > 'a' || strng[i] < 'z')
+        if (strng[i] > 'a' && strng[i] < 'z')
             strng[i] = strng[i] - 32;
     }
 }
 
-void encryptRotation(int x, char *strng)
-{
-    for(int i = 0; i < 30; i++)
-    {
-        strng[i] = strng[i] - x;
-    }
-    for(int i=0; i<30; i++)
-    {
-        if (strng[i] > 90)
-            strng[i] = strng[i] - 26;
-        if (strng[i] < 65)
-            strng[i] = strng[i] + 26;
-    }
-}
-
-void decryptRotation(int x, char *strng)
-{
-    for(int i = 0; i < 30; i++)
-    {
-        strng[i] = strng[i] + x;
-    }
-    for(int i=0; i<30; i++)
-    {
-        if (strng[i] > 90)
-            strng[i] = strng[i] - 26;
-        if ( strng[i] < 65)
-            strng[i] = strng[i] + 26;
-    }
-}
