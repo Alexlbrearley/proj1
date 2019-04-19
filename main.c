4d45ba25130ff2a5ec9b214728a4598b55478d59
Alex Brearley <alexlbrearley@gmail.com>
2019-04-19 23:09:58 +1000

    Main program now has fully functional rotation cipher

--- main.c
+++ main.c
@@ -1,40 +1,153 @@
 #include <stdio.h>
+/*fprintf(out, "%s", strng); //prints strng to output file*/
 
-int menu();
-void rotation();
-void substitution();
 
+/*---This function takes the input text and converts everything to capital---*/
+void textCaps(char *strng);
+
+/* ---This function takes a variable x to rotate the value for each character---*/
+void encryptRotation(int x, char *strng);
+
+/*---This function takes encrypted text and the known value it was rotated by to decode for original text---*/
+void decryptRotation(int x, char *strng);
+              
+/*---Main---*/
 int main()
 {
-    char selection;
-    printf("Welcome to Cipher program\n\n");
-    printf("This program has the following options:\n\n a)Rotation Cypher\n b)Substitution Cypher\n c)Decryption\n\n Please choose one:\n");
-    scanf("%c", &c);
-    while(c < 'a' || c > 'c')
+    int c;
+    printf("\n\n/---Welcome to Cipher program---/\n");
+    printf("To use this program, a file containing a message to encrypt or decrypt needs to be opened.\n\n");
+
+    /*---File I/O---*/
+    int i;
+    char strng[100];
+    char ch;
+    FILE *in, *out;
+    char inputName[30];
+    
+    printf("Enter input file name: ");
+    scanf("%s", inputName);
+    in = fopen(inputName, "r");
+    if (in == NULL)         //prints user friendly error message
     {
-        switch(c)
+        perror("ERROR : fopen()");
+        return 0;
+    }
+    out = fopen("output.txt", "w+");
+    if (in == NULL)         //prints user friendly error message
+    {
+        perror("ERROR : fopen()");
+        return 0;
+    }
+    for (i=0; i<100; i++) //reads file into strng (only single words, stops at first white space)
+    {
+        fscanf(in, "%c", &ch);
+        if ( (feof(in)) ) //not end of file
+            break;
+        strng[i] = ch;
+    }
+    printf("\nFile contains: %s\n", strng); //original file contents
+    
+    textCaps(strng); //convert to all caps
+    
+    printf("This program can perform following operations:\n\n 1. Rotation Cipher\n 2. Substitution Cipher\n 3. Decrypt Without Key\n Please enter an option:");
+    scanf("%d", &c);
+    /*---Rotation Program---*/
+    if (c == 1)
+    {
+        printf("\nEnter 1 to encrypt or 2 to decrypt: ");
+        scanf("%d", &c);
+        int x;
+        char z;
+        if (c == 1) //encrypt rotation
         {
-            case 'a':
-            case 'b':
-            case 'c':
-            case 'd':
-            default: printf("Invalid option, please enter a), b), c) or d) : \n", c);
+            printf("\nEnter a rotation amount: ");
+            scanf("%d", &x);
+            encryptRotation(x, strng);
+            printf("\nThe result is: %s\n", strng);
+            fprintf(out, "%s", strng);
+            printf("\nWould you like to overwrite %s? y or n: ", inputName);
+            scanf("%s", &z);
+            if (z == 'y')
+            {
+                in = fopen(inputName, "w");
+                fprintf(in, "%s", strng);
+            }
 
         }
-        
+        else if (c == 2) //decrypt rotation
+        {
+            printf("\nEnter the rotation amount to decrypt: ");
+            scanf("%d", &x);
+            decryptRotation(x, strng);
+            printf("\nThe result is: %s\n", strng);
+            fprintf(out, "%s", strng);
+            printf("\nWould you like to overwrite %s? y or n: ", inputName);
+            scanf("%s", &z);
+            if (z == 'y')
+            {
+                in = fopen(inputName, "w");
+                fprintf(in, "%s", strng);
+            }
+        }
+        else
+            printf("%d is not valid", c);
     }
+    /*---Substitution Program---*/
+    else if (c == 2)
+    {
+    }
+    else if (c==3)
+    {
+    }
+    else
+        printf("%d is invalid\n", c);
     
-return 0;
+    
+    /*---End Main---*/
+    fclose(in);
+    fclose(out);
+    return 0;
+}
+              
+/*---Functions---*/
+void textCaps(char *strng)
+{
+    for(int i=0; i<100; i++)
+    {
+        if (strng[i] >= 'a' && strng[i] <= 'z')
+            strng[i] = strng[i] - 32;
+    }
+}
+              
+void encryptRotation(int x, char *strng)
+{
+    for(int i = 0; i < 100; i++)
+    {
+        if (strng[i] >= 65 && strng[i] <=90)
+        {
+            strng[i] = strng[i] - x;
+        }
+    }
+    for(int i=0; i<100; i++)
+    {
+        if (strng[i] < 65 && strng[i] >= 65-x)
+            strng[i] = strng[i] + 26;
+    }
 }
 
-void rotation()
+void decryptRotation(int x, char *strng)
 {
-    
-    
-}
-
-void substitution()
-{
-    
-    
+    for(int i = 0; i < 100; i++)
+    {
+        if (strng[i] >= 65 && strng[i] <=90)
+        {
+            strng[i] = strng[i] + x;
+        }
+    }
+    for(int i=0; i<100; i++)
+    {
+        if (strng[i] > 90 && strng[i] <= 90+x)
+            strng[i] = strng[i] - 26;
+    }
 }
