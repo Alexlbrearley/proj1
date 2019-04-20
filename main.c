e92b008125cc411634573f1f89f4823487d98e71
Alex Brearley <alexlbrearley@gmail.com>
2019-04-20 19:04:41 +1000

    cleaning up

--- main.c
+++ main.c
@@ -1,22 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 
-/*fprintf(out, "%s", strng); //prints strng to output file*/
-
-
 /*---This function takes the input text and converts everything to capital---*/
 void textCaps(char *strng);
-
 /* ---This function takes a variable x to rotate the value for each character---*/
 void encryptRotation(int x, char *strng);
-
 /*---This function takes encrypted text and the known value it was rotated by to decode for original text---*/
 void decryptRotation(int x, char *strng);
-
-void encryptSubstitution(char *keyStrng, char *strng);
-
-void decryptSubstitution(char *keyStrng, char *strng);
-
+/* ---This function takes a key string and a message string to encode the message---*/
+void encryptSubstitution(char *key, char *strng);
+/* ---This function takes a key string and a message string to decode the message---*/
+void decryptSubstitution(char *key, char *strng);
               
 /*---Main---*/
 int main()
@@ -28,14 +22,12 @@ int main()
     printf("/---Welcome to Cipher program---/\n");
     printf("/-------------------------------/\n\n");
     printf("To use this program, a file containing a message to encrypt or decrypt needs to be opened.\n\n");
-
-    /*---File I/O---*/
+/*---File I/O---*/
     int i;
-    char strng[100];
+    char message[100];
     char ch;
     FILE *in, *out, *key;
     char inputName[30];
-    
     printf("Enter input file name: ");
     scanf("%s", inputName);
     in = fopen(inputName, "r");
@@ -50,21 +42,20 @@ int main()
         perror("ERROR : fopen()");
         return 0;
     }
-    
     for (i=0; i<100; i++) //reads file into strng
     {
         fscanf(in, "%c", &ch);
         if ( (feof(in)) ) //not end of file
             break;
-        strng[i] = ch;
+        message[i] = ch;
     }
     printf("\n\n/-------------------------------/");
-    printf("\nFile contains: %s", strng); //original file contents
-    textCaps(strng); //convert to all caps
+    textCaps(message); //convert to all caps
+    printf("\nFile contains: %s", message); //original file contents
     printf("\n/-------------------------------/\n");
     printf("\nThis program can perform following operations:\n\n 1. Rotation Cipher\n 2. Substitution Cipher\n 3. Decrypt Without Key\n\n Please enter an option: ");
     scanf("%d", &c);
-    /*---Rotation Program---*/
+/*---Rotation Program---*/
     if (c == 1)
     {
         printf("\nEnter 1 to encrypt or 2 to decrypt: ");
@@ -73,40 +64,39 @@ int main()
         {
             printf("\nEnter a rotation amount: ");
             scanf("%d", &x);
-            encryptRotation(x, strng);
-            printf("\nThe result is: %s\n", strng);
-            fprintf(out, "%s", strng);
-            printf("\nWould you like to overwrite %s? y or n: ", inputName); //option to overwrite the input file so that the decrypt function can be tested
+            encryptRotation(x, message);
+            printf("\nThe result is: %s\n", message);
+            fprintf(out, "%s", message);
+            printf("\nWould you like to overwrite %s with the encrypted text? y or n: ", inputName); //option to overwrite the input file so that the decrypt function can be tested
             scanf("%s", &z);
             if (z == 'y')
             {
                 in = fopen(inputName, "w");
-                fprintf(in, "%s", strng);
+                fprintf(in, "%s", message);
             }
-
         }
         else if (c == 2) //decrypt rotation
         {
             printf("\nEnter the rotation amount to decrypt: ");
             scanf("%d", &x);
-            decryptRotation(x, strng);
-            printf("\nThe result is: %s\n", strng);
-            fprintf(out, "%s", strng);
-            printf("\nWould you like to overwrite %s? y or n: ", inputName); //option to overwrite the input file
+            decryptRotation(x, message);
+            printf("\nThe result is: %s\n", message);
+            fprintf(out, "%s", message);
+            printf("\nWould you like to overwrite %s with the encrypted text? y or n: ", inputName); //option to overwrite the input file
             scanf("%s", &z);
             if (z == 'y')
             {
                 in = fopen(inputName, "w");
-                fprintf(in, "%s", strng);
+                fprintf(in, "%s", message);
             }
         }
         else
             printf("%d is not valid", c);
     }
-    /*---Substitution Program---*/
+/*---Substitution Program---*/
     else if (c == 2)
     {
-        char keyStrng[26];
+        char keyStrng[25];
         key = fopen("key.txt", "r");
         if (key == NULL)         //prints user friendly error message
         {
@@ -120,42 +110,37 @@ int main()
                 break;
             keyStrng[i] = ch;
         }
+        textCaps(keyStrng);
         
         printf("\nFound key: %s\n", keyStrng);
         printf("\nEnter 1 to encrypt or 2 to decrypt: ");
         scanf("%d", &c);
-        
         if(c == 1)
         {
-            encryptSubstitution(keyStrng, strng);
-            printf("\nThe encrypted message is: %s\n", strng);
-            printf("\nWould you like to overwrite %s? y or n: ", inputName); //option to overwrite the input file so that the decrypt function can be tested
+            encryptSubstitution(keyStrng, message);
+            printf("\nThe encrypted message is: %s\n", message);
+            printf("\nWould you like to overwrite %s? with the encrypted text y or n: ", inputName); //option to overwrite the input file so that the decrypt function can be tested
             scanf("%s", &z);
             if (z == 'y')
             {
                 in = fopen(inputName, "w");
-                fprintf(in, "%s", strng);
+                fprintf(in, "%s", message);
             }
         }
         else if(c == 2)
         {
-            decryptSubstitution(keyStrng, strng);
-            printf("\nThe encrypted message is: %s\n", strng);
-            printf("\nWould you like to overwrite %s? y or n: ", inputName); //option to overwrite the input file
+            decryptSubstitution(keyStrng, message);
+            printf("\nThe encrypted message is: %s\n", message);
+            printf("\nWould you like to overwrite %s with the encrypted text? y or n: ", inputName); //option to overwrite the input file
             scanf("%s", &z);
             if (z == 'y')
             {
                 in = fopen(inputName, "w");
-                fprintf(in, "%s", strng);
+                fprintf(in, "%s", message);
             }
         }
-        
-        
-        
-        
-        
     }
-    /*---newprogram---*/
+/*---newprogram---*/
     else if (c==3)
     {
     }
@@ -163,15 +148,13 @@ int main()
         printf("%d is invalid\n", c);
     
     
-    /*---End Main---*/
-    
+/*---End Main---*/
     fclose(in);
     fclose(out);
     fclose(key);
     printf("\n\n/-------------------------------/\n");
     printf("/------------Goodbye!-----------/");
     printf("\n/-------------------------------/\n\n");
-
     return 0;
 }
               
@@ -184,7 +167,7 @@ void textCaps(char *strng)
             strng[i] = strng[i] - 32;
     }
 }
-              
+//------------------------------------------------------
 void encryptRotation(int x, char *strng)
 {
     for(int i = 0; i < 100; i++)
@@ -200,7 +183,7 @@ void encryptRotation(int x, char *strng)
             strng[i] = strng[i] + 26;
     }
 }
-
+//------------------------------------------------------
 void decryptRotation(int x, char *strng)
 {
     for(int i = 0; i < 100; i++)
@@ -216,8 +199,8 @@ void decryptRotation(int x, char *strng)
             strng[i] = strng[i] - 26;
     }
 }
-
-void encryptSubstitution(char *keyStrng, char *strng)
+//------------------------------------------------------
+void encryptSubstitution(char *key, char *strng)
 {
     int i, l;
     char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -227,15 +210,15 @@ void encryptSubstitution(char *keyStrng, char *strng)
         {
             if (alphabet[l] == strng[i])
             {
-                strng[i] = keyStrng[l];
+                strng[i] = key[l];
                 break;
             }
             
         }
     }
 }
-
-void decryptSubstitution(char *keyStrng, char *strng)
+//------------------------------------------------------
+void decryptSubstitution(char *key, char *strng)
 {
     int i, l;
     char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -243,7 +226,7 @@ void decryptSubstitution(char *keyStrng, char *strng)
     {
         for (l=0; l<26; l++)
         {
-            if (keyStrng[l] == strng[i])
+            if (key[l] == strng[i])
             {
                 strng[i] = alphabet[l];
                 break;
